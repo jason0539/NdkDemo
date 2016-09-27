955b442e69e038ca5a09d8d5771a9515a0ebe75f
jason0539 <jason0539@163.com>
2016-09-27 22:56:17 +0800

    调用Jni (把运行生成的so放到app/src/main下的jniLibs中,之前的jni目录和build.gradle中的配置就不需要了)

--- app/src/main/java/com/lzh/ndkdemo/MainActivity.java
+++ app/src/main/java/com/lzh/ndkdemo/MainActivity.java
@@ -2,6 +2,10 @@ package com.lzh.ndkdemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.lzh.ndkdemo.jni.JniUtil;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_calljni).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((TextView)findViewById(R.id.tv_show)).setText(new JniUtil().test());
+            }
+        });
     }
 }
